d1ac37e10b18c145e299ff767cbba8e2abd74fde
Enrique Noriega <enrique.noriega@gmail.com>
2022-01-24 15:58:30 -0700

    Finished the weight panel

--- src/components/weight/WeightPanel.jsx
+++ src/components/weight/WeightPanel.jsx
@@ -1,45 +1,7 @@
-import { Form, Row, Col } from 'react-bootstrap';
+import SliderComponent from './SliderComponent';
+import LatexFormula from './LatexFormula';
 import { useState } from 'react';
 import  "./WeightPanel.css";
-import RangeSlider from 'react-bootstrap-range-slider';
-
-function SliderComponent({label, value, max, granularity, onChange}) {
-
-	if(!max)
-		max = 10;
-
-	if(!granularity)
-		granularity = .1;
-
-	return (
-		<Form>
-			<Form.Group as={Row}>
-				<Col>
-					<Form.Label>{label}:</Form.Label>
-				</Col>
-				<Col xs={9}>
-					<RangeSlider 
-						value={value}
-						min={1}
-						max={max}
-						step={granularity}
-						tooltip='off'
-						onChange={e => {
-							if(onChange)
-								onChange(e.target.value);
-							}}
-					/>
-				</Col>
-				<Col xs={1}>
-					<Form.Control type="number" value={value} onChange={e => {
-						if(onChange)
-							onChange(e.target.value);
-					}}/>
-				</Col>
-			</Form.Group>
-		</Form>
-	);
-}
 
 
 export default function WeightPanel({ sliderValues, setSliderValues }){
@@ -55,15 +17,20 @@ export default function WeightPanel({ sliderValues, setSliderValues }){
 
 	let elements;
 	if(isExpanded)
-		elements = (<div>
-			<SliderComponent label='Frequency' value={sliderValues.frequency} onChange={v => sliderStateUpdate('frequency', v)} />
-			<SliderComponent label='Has significance' value={sliderValues.hasSignificance} onChange={v => sliderStateUpdate('hasSignificance', v)} />
-			<SliderComponent label='Avg significance' value={sliderValues.avgSignificance} onChange={v => sliderStateUpdate('avgSignificance', v)} />
-			<SliderComponent label='Avg impact factor' value={sliderValues.avgImpactFactor} onChange={v => sliderStateUpdate('avgImpactFactor', v)} />
-			<SliderComponent label='Max impact factor' value={sliderValues.maxImpactFactor} onChange={v => sliderStateUpdate('maxImpactFactor', v)} />
-			<SliderComponent label='1 - Avg p-value' value={sliderValues.pValue} onChange={v => sliderStateUpdate('pValue', v)} />
-			
-		</div>);
+		elements = (
+			<>
+				<LatexFormula coefficients={sliderValues} />
+				<div>
+					<SliderComponent label='Frequency' value={sliderValues.frequency} onChange={v => sliderStateUpdate('frequency', v)} />
+					<SliderComponent label='Has significance' value={sliderValues.hasSignificance} onChange={v => sliderStateUpdate('hasSignificance', v)} />
+					<SliderComponent label='Avg significance' value={sliderValues.avgSignificance} onChange={v => sliderStateUpdate('avgSignificance', v)} />
+					<SliderComponent label='Avg impact factor' value={sliderValues.avgImpactFactor} onChange={v => sliderStateUpdate('avgImpactFactor', v)} />
+					<SliderComponent label='Max impact factor' value={sliderValues.maxImpactFactor} onChange={v => sliderStateUpdate('maxImpactFactor', v)} />
+					<SliderComponent label='1 - Avg p-value' value={sliderValues.pValue} onChange={v => sliderStateUpdate('pValue', v)} />
+					
+				</div>
+			</>
+		);
 	else
 		elements = <></>;
 
@@ -74,6 +41,7 @@ export default function WeightPanel({ sliderValues, setSliderValues }){
 					setExpanded(!isExpanded)
 				}}>
 			Weighting<span className="chevron">{chevron}</span></h2>
+			
 			{elements}
 		</div>
 	)
